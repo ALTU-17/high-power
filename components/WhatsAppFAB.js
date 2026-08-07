13f8bc8ea5c0358b80d4d02837561f57c3defd4c
altamash.shaikh@aceventura.in <altamash.shaikh@aceventura.in>
2026-08-07 22:10:21 +0530

    Updated hero page wa

--- components/WhatsAppFAB.js
+++ components/WhatsAppFAB.js
@@ -1,5 +1,5 @@
 'use client';
-
+import { FaWhatsapp } from 'react-icons/fa';
 import { MessageCircle } from 'lucide-react';
 import { shop } from '@/lib/data';
 
@@ -13,7 +13,7 @@ export default function WhatsAppFAB() {
       className="fixed bottom-5 right-5 z-40 w-14 h-14 rounded-full bg-[#25D366] flex items-center justify-center shadow-lg
        shadow-black/40 animate-pulseRing hover:scale-105 transition-transform"
     >
-      <MessageCircle className="text-white" size={26} strokeWidth={2.2} />
+      <FaWhatsapp className="w-7 h-7 text-white" />
     </a>
   );
 }
